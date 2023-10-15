4e7d127f673614c719a0f71f6f7a0f051d9578e4
jayanijenish <147860064+jayanijenish@users.noreply.github.com>
2023-10-15 13:26:13 +0000

    data entry

--- app.py
+++ app.py
@@ -1,8 +1,41 @@
 from flask import Flask, render_template
 app = Flask(__name__)
+
+jobs=[
+  {
+    "id": 1,
+    "title": "Data Analyst",
+    "location": "Mountain View, CA",
+    "salary": "$120,000"
+  },
+  
+  {
+    "id": 2,
+    "title": "Software Engineer",
+    "location": "San Francisco, CA",
+    "salary": "$120,000"
+  },
+  
+  {
+    "id": 3,
+    "title": "Data Scientist",
+    "location": "New York, NY",
+    "salary": "$120,000"
+  },
+  
+  {
+    "id": 4,
+    "title": "Data Engineer",
+    "location": "Los Angeles, CA",
+    "salary": "$120,000"
+  },
+]
+
 @app.route("/")
 def hello_world():
-  return render_template("home.html")
+  return render_template("home.html",
+                         jobs=jobs,
+                        company_name="om")
   
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
